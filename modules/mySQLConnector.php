690804f25d67e2f62acb932858a495f0e9ca9f0b
Tom Smith <tomsmith1204@users.noreply.github.com>
2014-12-15 19:52:44 -0500

    Update mySQLConnector.php

--- modules/mySQLConnector.php
+++ modules/mySQLConnector.php
@@ -2,9 +2,9 @@
 
 /*
 	*****CREDITS: PLEASE DO NOT DELETE*****
-	*		 Written by Thomas Smith      *
-	*		   	      2015                *
-	*		  tomsmith1204@gmail.com      *
+	*	 Written by Thomas Smith      *
+	*		  2015                *
+	*	 tomsmith1204@gmail.com       *
 	***************************************
 */	
 
